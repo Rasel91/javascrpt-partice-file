86b2f124f23bb699f69c18cd05a98a942daf40b0
Rasel91 <srasel.dpe@gmail.com>
2022-08-09 23:32:21 +0600

    wood.js add

---
+++ wood.js
@@ -0,0 +1,22 @@
+// 1. chair --> 3 cft
+//  2. table --> 10 cft 
+//  3. bed --> 50 cft
+// vary: quantity
+
+function woodcalculator(chairQuantity, tableQuantity, bedQuantity){
+
+    const perChairwood = 3;
+    const perTablewood = 10; 
+    const perBedwood = 50;
+
+    const chairwood = chairQuantity * perChairwood;
+    const tablewood = tableQuantity * perTablewood;
+    const bedewood = bedQuantity * perBedwood;
+
+    // console.log(chairQuantity, tableQuantity, bedQuantity);
+    const totalwood = chairwood + tablewood + bedewood;
+    return totalwood;
+}
+
+const totalwood = woodcalculator(4, 6, 5);
+console.log(totalwood);
