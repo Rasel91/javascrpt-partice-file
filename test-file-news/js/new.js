2c8c191db469f475eec90d815863eab3a4411660
Rasel91 <srasel.dpe@gmail.com>
2022-09-04 08:05:58 +0600

    cata id add

--- test-file-news/js/new.js
+++ test-file-news/js/new.js
@@ -22,9 +22,8 @@ function displayNews(news) {
         newsDiv.innerHTML = `
         <ul> 
         <li class="nav-item">
-       <a class="nav-link active" aria-current="page" href="#">${category.category_name} </a>
+        <a onclick="loadCatagoriesDetails(${category.category_id})" class="nav-link active" aria-current="page" href="#">${category.category_name} </a>
        </li>
-
        </ul>
         
         `;
@@ -37,10 +36,6 @@ loadNews();
 
 // ===============
 
-
-
-
-
 const loadNewsPart = async () => {
     const url = ('https://openapi.programming-hero.com/api/news/category/01')
     const res = await fetch(url)
@@ -48,11 +43,11 @@ const loadNewsPart = async () => {
     // console.log(data)
     displayNewsPart(data.data);
     // console.log(data.data);
-}
+};
 
 
 function displayNewsPart(information) {
-    console.log(information);
+    // console.log(information);
     const newsPartContainer = document.getElementById('newspart-container');
     information.forEach(info => {
         const newPartDiv = document.createElement('div');
@@ -61,7 +56,7 @@ function displayNewsPart(information) {
         newPartDiv.classList.add('user');
         newPartDiv.innerHTML = `
 
-       <div class= 'd-flex' >
+       <div class= 'd-flex mb-5px border-danger  ' >
             <div class="col-md-4">
                  <img src="${info.image_url}" class="img-fluid rounded-start" alt="...">
             </div>
@@ -70,18 +65,9 @@ function displayNewsPart(information) {
                     <h5 class="card-title">${info.title}</h5>
                     <p class="card-text">${info.details}</p>
                 </div>
-            </div> 
-               
-       </div>
-
-
-       <div class= 'd-flex' >
-       <div class="col-md-4">
-
-       </div>
-       <div class="col-md-8">
-       <div class="d-flex justify-content-evenly" >
-       <div class="d-flex align-items-center " >
+              <div> 
+              <div class="d-flex justify-content-evenly" >
+          <div class="d-flex align-items-center " >
            <img class='w-25 rounded-4 img-size m-2'  src="${info.author.img}" alt="">
            <div>
                 <h5>${info.author.name}</h5>
@@ -99,21 +85,37 @@ function displayNewsPart(information) {
                 </div>
     
         </div>
+
       <button class="btn btn-primary btn-size">See More</button>  
-</div>
+              
+              
+              
+              </div>
 
+            </div> 
+               
+       </div>
+
+
+       <div class= 'd-flex' >
+       <div class="col-md-4">
+
+       </div>
+       <div class="col-md-8">
+       
+     </div>
        </div>
        </div>
        
-
-    
-       
-  
-        `;
-
+    `;
         newsPartContainer.appendChild(newPartDiv);
     });
 }
+
+const loadCatagoriesDetails = (category_id) => {
+    console.log(category_id);
+}
+
 
 loadNewsPart();
 
